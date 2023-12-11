2fa5756e03570e78d6a36e6d3e93cdf2f0486db8
Fabian Miiro <iloabn@users.noreply.github.com>
2023-12-11 07:27:21 +0100

    Publish dates for 2024 (#35)
    Adding all 2024 dates.
    
    ---------
    
    Co-authored-by: iloabn <5629868+iloabn@users.noreply.github.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,23 +1,23 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_includes/bundle.css");
     eleventyConfig.addPassthroughCopy("_includes/bg.svg");
     eleventyConfig.ignores.add("README.md");
 
-    eleventyConfig.addCollection("upcomingDojos", function(collectionApi) {
-        let filteredCollection = collectionApi.getFilteredByTag("dojo").filter(function(item) {
+    eleventyConfig.addCollection("upcomingDojos", function (collectionApi) {
+        let filteredCollection = collectionApi.getFilteredByTag("dojo").filter(function (item) {
             return new Date(item.fileSlug) > Date.now();
         })
 
-        filteredCollection.sort((a,b) => {
+        filteredCollection.sort((a, b) => {
             return new Date(a.fileSlug) - new Date(b.fileSlug);
         });
-        return filteredCollection;
+        return filteredCollection.slice(0, 10);
     });
 
-    eleventyConfig.addFilter("betterDate", function(value) {
+    eleventyConfig.addFilter("betterDate", function (value) {
         let rawDate = new Date(value);
-        return rawDate.getUTCFullYear() + "-" + 
-        (rawDate.getUTCMonth()+1).toString().padStart(2, '0') + "-" +
-        rawDate.getUTCDate().toString().padStart(2, '0'); 
+        return rawDate.getUTCFullYear() + "-" +
+            (rawDate.getUTCMonth() + 1).toString().padStart(2, '0') + "-" +
+            rawDate.getUTCDate().toString().padStart(2, '0');
     });
 };
